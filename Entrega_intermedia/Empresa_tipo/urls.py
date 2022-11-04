7bd4b5bfc85d6a746bf625c4ddaf7009643eef5e
Vittorio Costanzi <vittoriogc96@gmail.com>
2022-11-04 20:08:55 -0300

    Se añadieron templates, herencia de templates, se añadio el formulario en la pestaña contacto, se linkearon los botones de la barra de navegacion etc

--- Entrega_intermedia/Empresa_tipo/urls.py
+++ Entrega_intermedia/Empresa_tipo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import inicio, nosotros, proyectos, contacto
 
 urlpatterns = [
-    path('inicio/', inicio),
-    path('nosotros/', nosotros),
-    path('proyectos/', proyectos),
-    path('contacto/', contacto),
+    path('inicio/', inicio, name="Inicio"),
+    path('nosotros/', nosotros, name="Nosotros"),
+    path('proyectos/', proyectos, name="Proyectos"),
+    path('contacto/', contacto, name="Contacto"),
 ]
